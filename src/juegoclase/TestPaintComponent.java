b234f09b1d1ae9cdd1260c714c9f2656d5858f18
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-05-10 15:05:05 -0500

    Juego

--- src/juegoclase/TestPaintComponent.java
+++ src/juegoclase/TestPaintComponent.java
@@ -8,8 +8,11 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javafx.scene.shape.Circle;
 import javax.swing.JPanel;
 
 public class TestPaintComponent extends JFrame{
@@ -31,27 +34,30 @@ class NewPanel extends JPanel implements ActionListener, MouseListener{
     private int y;
     private Timer timer;
     private int secuencia;
+    private int z;
 
     public NewPanel() {
         this.addMouseListener(this);
+        addKeyListener(new TAdapter());
+        setFocusable(true);
         timer= new Timer(70, this);
         timer.start();
     }
     
     public Rectangle getBounds(){
-        return new Rectangle(x+15, 370, 95, 65);
+        return new Rectangle(x, y+385, 32, 32);
     }
     
     public void checkCollision(){
     Rectangle tanque= getBounds();
     Rectangle objeto= new Rectangle(400, 398, 20, 20);
-    if(tanque.intersects(objeto)){
+    Rectangle objeto2= new Rectangle(20, z+0, 20, 50); 
+    if(tanque.intersects(objeto)||tanque.intersects(objeto2)){
         System.out.print("Colision");
         timer.stop();
     }
     
 }
-    
     @Override
     protected void paintComponent(Graphics g){
         
@@ -60,9 +66,10 @@ class NewPanel extends JPanel implements ActionListener, MouseListener{
         g.drawImage(fondo, 0, 0, null);
         
         Image azulito= loadImage("free_radical_game_sprites.png");
-        g.drawImage(azulito, x, 360, x+32, 392, 192+(this.secuencia*32)+32, 192-32,192+(this.secuencia*32), 192, this);
+        g.drawImage(azulito, x, y+385, x+32, y+417, 192+(this.secuencia*32)+32, 192-32,192+(this.secuencia*32), 192, this);
         
         Image moneda= loadImage("FullCoins.png");
+        //g.drawImage(moneda, );
 //        Image gato= loadImage("cats.gif");
 //        g.drawImage(gato, x, 360, x+132, 440, (this.secuencia*132), 0, (this.secuencia*132)+132, 80, this);
         
@@ -77,11 +84,11 @@ class NewPanel extends JPanel implements ActionListener, MouseListener{
         g.fillRect(400, 398, 20, 20);
         
         g.setColor(Color.blue);
-        g.fillRect(20, y+0, 20, 50);
-        g.fillOval(120, y+0, 30, 30);
+        g.fillRect(20, z+0, 20, 50);
+        g.fillOval(120, z+0, 30, 30);
       
         g.setColor(Color.black);
-        g.drawRect(x+15, 370, 95, 65);
+        g.drawRect(x, y+385, 32, 32);
     }
     public Image loadImage (String imageName){
         ImageIcon ii= new ImageIcon(imageName);
@@ -91,20 +98,47 @@ class NewPanel extends JPanel implements ActionListener, MouseListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(x<900){
-         x+=5;
-        }
+//        if(x<900){
+//         x+=5;
+//        }
         if(this.secuencia ==2){
             this.secuencia=0;
         }else{
             this.secuencia++;
         }
-//        if(y<500){
-         y+=5;
+        if(z<500){
+            z+=5;
         
-//    }
+   }
         checkCollision();
         repaint();
+    }
+    
+    private class TAdapter extends KeyAdapter{
+        public void keyReleased(KeyEvent e){
+            System.out.println("Soltó el botón");
+        }
+        public void keyPressed(KeyEvent e){
+            System.out.println("Presionó el botón");
+            int key= e.getKeyCode();
+            if(key==KeyEvent.VK_SPACE){
+                System.out.println("Presiono el espacio");
+            }
+            if(key==KeyEvent.VK_LEFT){
+                x=x-5;
+            }
+            if(key==KeyEvent.VK_RIGHT){
+                x=x+5;
+            }
+            if(key==KeyEvent.VK_UP){
+                y=y-5;
+            }
+            if(key==KeyEvent.VK_DOWN){
+                y=y+5;
+            }
+            
+        }
+      
     }
 
     @Override
